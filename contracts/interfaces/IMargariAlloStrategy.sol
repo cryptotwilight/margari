f9aa6aac27951ca81e80d2b65d3fde806bc2fb2b
Cryptotwilight <tonyusheuk@gmail.com>
2024-07-06 21:07:56 +0100

    Updated

--- contracts/interfaces/IMargariAlloStrategy.sol
+++ contracts/interfaces/IMargariAlloStrategy.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: APACHE 2.0
 pragma solidity ^0.8.23;
 
+struct ProjectFinancials{
+    address project; 
+    uint256 allocation; 
+    uint256 remainingAllocation; 
+    uint256 totalPaidOut;
+}
 
 interface IMargariAlloStrategy { 
 
+    function getProjectFinancials(address _project) view external returns (ProjectFinancials memory _financials);
+
+    function getProjects() view external returns (address [] memory _projects);
 
 }
